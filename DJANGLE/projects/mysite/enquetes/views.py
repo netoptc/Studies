80e0bb950e68d57a63236a4328e92e80c612dc49
netoptc <joaoq_neto09@hotmail.com>
2022-05-24 10:33:37 -0300

    remove folder 'djangle'

--- DJANGLE/projects/mysite/enquetes/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, world. Você esta nas enquetes.")
-
-
-# Create your views here.
--- DJANGLE/projects/tutorialdjango/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post;
-
-#Adicionando o Post a tela de admin
-admin.site.register(Post) #mesma função @admin.register(Post)
-
